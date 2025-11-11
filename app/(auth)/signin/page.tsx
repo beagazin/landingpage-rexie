e3e0272cef25c45d368fbb8071afc9f3517a4653
beagazin <lucasmachado.pessoal@gmail.com.br>
2025-11-11 12:08:05 -0300

    att

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -13,7 +13,7 @@ export default function SignIn() {
           {/* Section header */}
           <div className="pb-12 text-center">
             <h1 className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text font-nacelle text-3xl font-semibold text-transparent md:text-4xl">
-              Welcome back
+              Bem-vindo de volta!
             </h1>
           </div>
           {/* Contact form */}
@@ -24,13 +24,13 @@ export default function SignIn() {
                   className="mb-1 block text-sm font-medium text-indigo-200/65"
                   htmlFor="email"
                 >
-                  Email
+                  E-mail
                 </label>
                 <input
                   id="email"
                   type="email"
                   className="form-input w-full"
-                  placeholder="Your email"
+                  placeholder="Seu e-mail"
                 />
               </div>
               <div>
@@ -39,40 +39,40 @@ export default function SignIn() {
                     className="block text-sm font-medium text-indigo-200/65"
                     htmlFor="password"
                   >
-                    Password
+                    Senha
                   </label>
                   <Link
                     className="text-sm text-gray-600 hover:underline"
                     href="/reset-password"
                   >
-                    Forgot?
+                    Esqueci minha senha
                   </Link>
                 </div>
                 <input
                   id="password"
                   type="password"
                   className="form-input w-full"
-                  placeholder="Your password"
+                  placeholder="Sua senha"
                 />
               </div>
             </div>
             <div className="mt-6 space-y-5">
               <button className="btn w-full bg-linear-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_--theme(--color-white/.16)] hover:bg-[length:100%_150%]">
-                Sign in
+                Acesse sua conta
               </button>
               <div className="flex items-center gap-3 text-center text-sm italic text-gray-600 before:h-px before:flex-1 before:bg-linear-to-r before:from-transparent before:via-gray-400/25 after:h-px after:flex-1 after:bg-linear-to-r after:from-transparent after:via-gray-400/25">
-                or
+                ou
               </div>
               <button className="btn relative w-full bg-linear-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,var(--color-gray-800),var(--color-gray-700),var(--color-gray-800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%]">
-                Sign In with Google
+                Acesse com o Google
               </button>
             </div>
           </form>
           {/* Bottom link */}
           <div className="mt-6 text-center text-sm text-indigo-200/65">
-            Don't you have an account?{" "}
+            Ainda não tem uma conta?{" "}
             <Link className="font-medium text-indigo-500" href="/signup">
-              Sign Up
+              Cadastra-se agora!
             </Link>
           </div>
         </div>
